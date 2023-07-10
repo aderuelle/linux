6f9119da7761abe5be17e8ba0b069d55cc1a0842
Alexis Deruelle <alexis.deruelle@gmail.com>
2023-07-10 13:42:18 +0200

    rust:macos-build:host macro crate path computation
    Use rustc compile printing option to get host macros crate path.
    
    The rust-analyzer JSON configuration should take into account the actual
    macro_crate naming for the host: *.dylib or *.so
    
    Signed-off-by: Alexis Deruelle <alexis.deruelle@gmail.com>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -8,6 +8,8 @@ import json
 import logging
 import pathlib
 import sys
+import os
+import subprocess
 
 def generate_crates(srctree, objtree, sysroot_src):
     # Generate the configuration list.
@@ -65,7 +67,15 @@ def generate_crates(srctree, objtree, sysroot_src):
         [],
         is_proc_macro=True,
     )
-    crates[-1]["proc_macro_dylib_path"] = "rust/libmacros.so"
+
+    rustc = os.getenv("RUSTC", "rustc")
+    libmacros_path = subprocess.run(
+        f"{rustc} --print file-names --crate-name macros --crate-type proc-macro -".split(),
+        stdin=subprocess.DEVNULL,
+        capture_output=True
+    ).stdout.decode().strip()
+    libmacros_path = f"rust/{libmacros_path}"
+    crates[-1]["proc_macro_dylib_path"] = libmacros_path
 
     append_crate(
         "build_error",
